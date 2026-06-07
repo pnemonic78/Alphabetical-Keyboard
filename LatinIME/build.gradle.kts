d5e51ad3b35165d6270fbc2aced3601789c21885
Moshe W <pnemonic@gmail.com>
2026-06-07 16:50:41 +0300

    app 3.01

--- LatinIME/build.gradle.kts
+++ LatinIME/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 37
-        versionCode = 300
-        versionName = "3.00"
+        versionCode = 301
+        versionName = "3.01"
 
         applicationId = "com.github.inputmethod.alphabetical"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
